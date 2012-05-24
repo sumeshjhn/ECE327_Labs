8adcdd439e108474c99c4d28673bd06f3e10e820
Sumesh John <sumeshjhn@gmail.com>
2012-05-24 16:48:00 -0400

    updated with fir_tb.vhd

--- lab2/fir_tb.vhd
+++ lab2/fir_tb.vhd
@@ -14,11 +14,38 @@ end entity;
 ------------------------------------------------------------------------
 
 architecture main of fir_tb is
-
-  
+	signal i_data	: word;
+	signal clock	: std_logic; 	
+	signal o_data	: word;
 begin
-
-  
+	
+	uut : entity work.fir(avg)
+    port map (
+      i_data => i_data,
+      clk => clock,
+      o_data => o_data
+    );
+ 
+	process 
+		begin
+			clock <= '0';
+			wait for 5 ns;
+			clock <= '1';
+			wait for 5 ns;
+		end process;
+	
+	
+	process 
+		begin
+			i_data <= x"0000";
+			wait until rising_edge(clock);
+			wait until rising_edge(clock);
+			wait until rising_edge(clock);
+			i_data <= x"0010";
+			wait until rising_edge(clock);
+			wait until rising_edge(clock);
+			wait until rising_edge(clock);
+	end process;
 end architecture;
 ------------------------------------------------------------------------
 
